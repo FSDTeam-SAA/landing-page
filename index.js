5f0dce154234715ef6978bb38219621f24a58117
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2026-04-02 20:17:34 +0600

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+function openMenu(){
+  document.getElementById("menuOverlay").classList.add("active");
+}
+
+function closeMenu(){
+  document.getElementById("menuOverlay").classList.remove("active");
+}
